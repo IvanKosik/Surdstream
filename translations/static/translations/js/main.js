aed3e3a567d7360d8b784b46dea0730ec9d9c590
alexionby <alexionon@mail.ru>
2018-01-24 22:20:12 +0300

    sliding menu

--- translations/static/translations/js/main.js
+++ translations/static/translations/js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
+
     $(".publication").click(function(){
-        console.log($(this).find('.video_placeholder'));
         $(this).find('.video_placeholder').slideToggle('slow');
     });
+
+    $("#logInButton").click( function(){
+        $("#search").removeClass("searchVisibleFromRight").removeClass("searchHiddenToRight");
+        $("#logIn").removeClass("logInHidden").addClass("logInVisible");
+        $("#search").removeClass("searchVisibleFromLeft").addClass("searchHiddenToLeft");
+    });
+
+    $("#closeLogInButton").click( function() {
+        $("#logIn").removeClass("logInVisible").addClass("logInHidden");
+        $("#search").addClass("searchVisibleFromLeft").removeClass("searchHiddenToLeft");
+    });
+
+    $("#closeSignInButton").click( function() {
+      $("#signIn").removeClass("signInVisible").addClass("signInHidden");
+      $("#search").addClass("searchVisibleFromRight").removeClass("searchHiddenToRight");
+    });
+
+    $("#signInButton").click( function(){
+        $("#search").removeClass("searchVisibleFromLeft").removeClass("searchHiddenToLeft");
+        $("#signIn").removeClass("signInHidden").addClass("signInVisible");
+        $("#search").removeClass("searchVisibleFromRight").addClass("searchHiddenToRight");
+    });
+
 });
