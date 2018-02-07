cf7124f2c003f84e26c4dc999e722cfb6aba304d
Ivan Kosik <ivankosik91@gmail.com>
2018-02-07 20:46:11 +0300

    Changed settings for static files.

--- surdstream/settings.py
+++ surdstream/settings.py
@@ -126,8 +126,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/'files',
-STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
+#%STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTH_USER_MODEL = 'translations.User'
 
