3bac89cd2dde8ad702c62721334df82070c5d7e2
alexionby <alexionon@mail.ru>
2018-02-09 11:46:33 +0300

    added auth to new intf

--- translations/templatetags/translation_extras.py
+++ translations/templatetags/translation_extras.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter('add_class')
 def add_class(value, arg):
     return value.as_widget(attrs={'class': arg})
+
+
+@register.filter('add_placeholder')
+def add_placeholder(value, arg):
+    return value.as_widget(attrs={'placeholder': arg})
