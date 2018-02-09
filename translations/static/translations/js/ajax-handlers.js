5a6213823ad15f0396bd4a6c7bfc6b4880cbd678
alexionby <alexionon@mail.ru>
2018-02-09 16:16:25 +0300

    Video add butoon and form

--- translations/static/translations/js/ajax-handlers.js
+++ translations/static/translations/js/ajax-handlers.js
@@ -13,10 +13,12 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
+
 $.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
--- translations/static/translations/js/new-index.js
+++ translations/static/translations/js/new-index.js
@@ -1,27 +1,61 @@
 $(function() {
-    console.log( "ready!" );
+
+    $(function () {
+
+      $('[data-toggle="tooltip"]').tooltip()
+
+      /* CUSTOMIZED TOOLTIP
+      $('[data-toggle="tooltip"]').tooltip({
+        'template':'<div class="tooltip" role="tooltip"> \
+         <div class="arrow"></div> \
+         <div class="tooltip-inner" style="background-color:green;"></div> \
+         </div>'})
+      */
+    })
 
     $('.navbar-nav a').click(function(e){
       e.preventDefault();
       if ( $(this).hasClass('active') ) {
-        console.log('ok');
       } else {
         $('.navbar-nav a').removeClass('active');
         $(this).addClass('active');
         $('main').children().hide();
-
         $($(this).attr('href')).show();
-
-        console.log($(this).attr('href'));
-
         $('.collapse').collapse('hide')
       }
     });
 
-    /*
-    $('a[href$="home"]').click(function(e) {
-      e.preventDefault();
 
-      $('#page-menu').toggle().toggleClass('active');
-    })*/
+    $('#addVideoBtn').click(function(e) {
+
+      console.log( $(this).html() )
+
+      $(this).children().toggleClass('fa-plus').toggleClass('fa-times');
+      $(this).toggleClass('btn-success').toggleClass('btn-danger');
+      $(this).blur();
+
+      if ( $(this).attr('data-original-title') !== 'Close') {
+        $('[data-toggle="tooltip"]').tooltip('dispose');
+        $(this).attr('title','Close');
+        $('[data-toggle="tooltip"]').tooltip({
+          'template':'<div class="tooltip" role="tooltip"> \
+           <div class="arrow red"></div> \
+           <div class="tooltip-inner red"></div> \
+           </div>'});
+      } else {
+        $('[data-toggle="tooltip"]').tooltip('dispose');
+        $(this).attr('title','Add');
+        $('[data-toggle="tooltip"]').tooltip({
+          'template':'<div class="tooltip" role="tooltip"> \
+           <div class="arrow"></div> \
+           <div class="tooltip-inner"></div> \
+           </div>'});
+      }
+
+      $("button.navbar-toggler").prop('disabled', !$('button.navbar-toggler').prop('disabled') );
+      $("main").toggle();
+      $("section#addVideo").toggleClass('d-none');
+
+    });
+
 });
